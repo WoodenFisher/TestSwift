cdc52b49626824cccf7e3f528c38ebe082cf69a2
奥山 学 <okuyamamanabu@okuyama-manabu-no-imac.local>
2014-06-23 01:46:39 +0900

    Initial Commit

---
+++ TwitterTimelineSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterTimelineSample
+//
+//  Created by 奥山 学 on 2014/06/23.
+//  Copyright (c) 2014年 奥山 学. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
