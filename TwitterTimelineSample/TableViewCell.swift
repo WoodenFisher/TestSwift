0f509b3b449ce1b0f3b2311c13cadf68e4d2f190
奥山 学 <okuyamamanabu@okuyama-manabu-no-imac.local>
2015-04-19 02:39:15 +0900

    first commit

---
+++ TwitterTimelineSample/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  TwitterTimelineSample
+//
+//  Created by 奥山 学 on 2014/06/23.
+//  Copyright (c) 2014年 奥山 学. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell{
+    
+    @IBOutlet var tweet : UILabel
+    
+}
